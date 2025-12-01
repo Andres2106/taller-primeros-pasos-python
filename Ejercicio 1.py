6ae9a3d6a252694ebab18a0dde5f00184fa435b4
Andres2106 <andresfelipebg2106@gmail.com>
2025-12-01 09:05:56 -0400

    Add files via upload

---
+++ Ejercicio 1.py
@@ -0,0 +1,23 @@
+#**********  Zona de Función  ***********#
+
+def definir_altura():
+    altura_t = 12
+    return altura_t
+
+def definir_base():
+    base_t = 15
+    return base_t
+
+def calcular_area(base, altura):
+    area = (base*altura)/2
+    return area
+
+def imprimir_resultado(resultado_area):
+    print("El área del triángulo es: " + str(resultado_area))
+
+#**************** Codigo Principal Python ****************#
+
+base = definir_base()
+altura = definir_altura()
+area = calcular_area(base, altura)
+resultado= imprimir_resultado(area)
