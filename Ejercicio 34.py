46718f6af0adf7cf3942f08b4322c8b41a105aec
Andres2106 <andresfelipebg2106@gmail.com>
2025-12-03 17:53:03 -0500

    Add files via upload

---
+++ Ejercicio 34.py
@@ -0,0 +1,23 @@
+#************ Zona de Funciones ************#
+
+def pedir_precio():
+    precio = float(input("Ingrese el precio: "))
+    return precio
+
+def definir_descuento():
+    descuento = 0.1
+    return descuento
+
+def calcular_valor(precio, descuento):
+    descuento = (precio * descuento)
+    valor_final = (precio - descuento)
+    return valor_final
+
+def Imprimir_resultado(resultado_valor):
+    print("El valor final con descuento es: ", str(resultado_valor))
+
+#************ Codigo Principal de Python ************#
+precio = pedir_precio()
+descuento = definir_descuento()
+valor_final = calcular_valor(precio, descuento)
+resultado = Imprimir_resultado(valor_final)
