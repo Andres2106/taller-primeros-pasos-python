44d0586b0be7d45496ee9496f552d06a67d60eba
Andres2106 <andresfelipebg2106@gmail.com>
2025-12-03 18:18:17 -0500

    Add files via upload

---
+++ Ejercicio 36.py
@@ -0,0 +1,24 @@
+#************ Zona de Funciones ************#
+    
+def pedir_entero1():
+    entero1 = int(input("Ingrese el primer número entero: "))
+    return entero1
+
+def pedir_entero2():
+    entero2 = int(input("Ingrese el segundo número entero: "))
+    return entero2
+
+def calcular_division(entero1, entero2):
+    if entero1 == 0:
+        print("No se puede dividir por cero.")
+    else:
+        cociente = (entero1 // entero2)
+
+def Imprimir_resultado(resultado_division):
+    print("El cociente de la división de ambos enteros es: ", str(resultado_division))
+     
+#************ Codigo Principal de Python ************#
+entero1 = pedir_entero1()
+entero2 = pedir_entero2()
+cociente = calcular_division(entero1, entero2)
+resultado = Imprimir_resultado(cociente)
