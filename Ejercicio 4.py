ebf7d4b1a7432cf38fd92da1ae6b165b0567f77d
Andres2106 <andresfelipebg2106@gmail.com>
2025-12-05 07:44:52 -0500

    Add files via upload

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -1,34 +1,11 @@
-#**********  Zona de Función  ***********#
+#*************** Zona de Funciones **************#
+suma = 0
+for i in range(0,10):
+    numero = int(input("Digite el numero " +str(i+1)+": "))
+    suma = suma +  numero
 
-def definir_pi():
-    pi=3.1416
-    return pi
+def Imprimir_resultado(resultado_suma):
+    print("La sumatoria total es: " +str(resultado_suma))
 
-
-def definir_radio():
-    radio= 45
-    return radio
-
-
-def definir_altura():
-    altura= 26
-    return altura
-
-
-def calcular_volumen(pi, radio, altura):
-    volumen= (pi* radio**2 * altura)
-    return volumen
-
-
-def imprimir_resultado(resultado_volumen):
-    print("El volumen del cilindro es: " +str(resultado_volumen))
-
-
-#**************** Codigo Principal Python ****************#
-
-
-pi= definir_pi()
-radio= definir_radio()
-altura= definir_altura()
-volumen= calcular_volumen(pi, radio, altura)
-resultado= imprimir_resultado(volumen)    
+#*************** Código Principal de Python ***************#
+resultado = Imprimir_resultado(suma)
